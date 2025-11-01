c1820846a597cc74bde72d0415ab2e4d7c31113f
abdulwasay903211 <abdulwasay903211@gmail.com>
2025-11-01 20:29:40 +0500

    Abdul Wasay | Logo on tob bar

--- src/app/components/Topbar.jsx
+++ src/app/components/Topbar.jsx
@@ -3,14 +3,18 @@ import React from "react";
 
 const Topbar = () => {
   return (
-    <div className="w-full bg-gray-100 text-gray-800 flex items-center justify-between px-8 py-5 shadow-md">
+    <div className="w-full bg-gray-100 text-gray-800 flex items-center justify-between px-12 py-5 shadow-md">
       {/* Left: Logo */}
       <div className="flex items-center space-x-2">
-        
+        <img
+          src="/logo.png" // place your logo in /public
+          alt="Logo"
+          className="w-30 h-13"
+        />
       </div>
 
       {/* Right: Title */}
-      <h1 className="text-xl font-bold tracking-wide text-right">
+      <h1 className="text-2xl font-bold tracking-wide text-right">
         Pay Order Management System
       </h1>
     </div>
